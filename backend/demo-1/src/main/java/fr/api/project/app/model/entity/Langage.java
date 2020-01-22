49aec52b88981566b6357d7f61059b480222ea9e
Elodie <elodie.pessy@gmail.com>
2020-01-22 11:53:13 +0100

    mise à jour mapping

--- backend/demo-1/src/main/java/fr/api/project/app/model/entity/Langage.java
+++ backend/demo-1/src/main/java/fr/api/project/app/model/entity/Langage.java
@@ -1,9 +1,12 @@
 package fr.api.project.app.model.entity;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /*
  * Correspond à la table des langages
@@ -13,6 +16,7 @@ public class Langage {
 
 	private int id;
 	private String langage;
+	private Collection <Question> question;
 	
 	
 	/*
@@ -27,12 +31,16 @@ public class Langage {
 	}
 	
 	public Langage(int id, String langage) {
-		super();
 		this.id = id;
 		this.langage = langage;
 	}
-
 	
+	public Langage(int id, String langage, Collection<Question> question) {
+		this.id = id;
+		this.langage = langage;
+		this.question = question;
+	}
+
 	/*
 	 * Méthodes getters and setters
 	 */
@@ -50,6 +58,17 @@ public class Langage {
 	public void setLangage(String langage) {
 		this.langage = langage;
 	}
+
+	@OneToMany
+	public Collection<Question> getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Collection<Question> question) {
+		this.question = question;
+	}
+	
+	
 	
 	
 }
--- backend/demo-1/src/main/java/fr/api/project/app/model/entity/Ranking.java
+++ backend/demo-1/src/main/java/fr/api/project/app/model/entity/Ranking.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 /*
  * Correspond au classement des utilisateurs
@@ -14,6 +15,7 @@ public class Ranking {
 	
 	private int id;
 	private int point;
+	private User user;
 	
 	/*
 	 * Constructeurs
@@ -31,7 +33,13 @@ public class Ranking {
 		this.point = point;
 	}
 
-	
+	public Ranking(int id, int point, User user) {
+		super();
+		this.id = id;
+		this.point = point;
+		this.user = user;
+	}
+
 	/*
 	 * Méthodes getters and setters
 	 */
@@ -52,6 +60,16 @@ public class Ranking {
 	public void setPoint(int point) {
 		this.point = point;
 	}
+
+	@OneToOne
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 	
 	
 }
