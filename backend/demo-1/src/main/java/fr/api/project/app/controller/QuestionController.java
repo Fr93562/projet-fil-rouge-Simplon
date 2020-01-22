1c5264bcf3c6f72bbfbe5d42f78b078945ccffba
Camille <camille.paudrat.lp4@gmail.com>
2020-01-22 18:26:30 +0100

    Question controller la liste des questions par langage doit etre fait coté langageController | Test effectué complémentaire data.sql est bien executé au demarrage de Springboot

--- backend/demo-1/src/main/java/fr/api/project/app/controller/QuestionController.java
+++ backend/demo-1/src/main/java/fr/api/project/app/controller/QuestionController.java
@@ -1,10 +1,93 @@
 package fr.api.project.app.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import fr.api.project.app.model.entity.Question;
+import fr.api.project.app.repository.QuestionRepository;
+
 public class QuestionController {
 	
+	@Autowired
+	private QuestionRepository questionRepository;
+
+
+
 	/**
-	 * Commentaires: CRUD (+ readAll)
-	 * test commit
+	 * Ajoute une question
+	 * @param newQuestion La question à ajouter
+	 * @return La question ajouté
+	 */
+	@PostMapping("/question")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Question AddQuestion(@RequestBody Question newQuestion) {
+
+		return questionRepository.saveAndFlush(newQuestion);
+	}
+
+	/**
+	 * Pas de question a faire dans langage
 	 */
 
+	/**
+	 * Renvoie toutes les questions au format Json 
+	 * 
+	 * @return : renvoie une liste en Json
+	 */
+	@GetMapping("/question")
+	public List<Question> readAll() {
+
+		return questionRepository.findAll();
+	}
+
+	/**
+	 * Mets a jour une question
+	 * 
+	 * @param modifQuestion
+	 * @return : une reponse en fonction de l'existence
+	 */
+	@PutMapping("/question")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String updateQuestion(@RequestBody Question modifQuestion) {
+
+		String output = "Question not found";
+		Optional<Question> verify = questionRepository.findById(modifQuestion.getId());
+
+		  if(verify.isPresent()) {
+
+			  questionRepository.saveAndFlush(modifQuestion);			  
+			  output = "Question has been update";
+		  }
+
+		  return output;
+	}
+
+	/**
+	 * Supprime une question
+	 * 
+	 * @param delQuestion : corresponds au Json transformé en objet question
+	 */
+	@DeleteMapping("/question")
+	@ResponseStatus(code = HttpStatus.OK)
+	public String delete(@RequestBody Question delQuestion) {
+
+		String output = "Question not found";
+		Optional<Question> verify = questionRepository.findById(delQuestion.getId());
+
+		if(verify.isPresent()) {
+
+			questionRepository.delete(delQuestion);	
+			output = "Question has been delete";
+		}
+		return output;
+	}
 }
