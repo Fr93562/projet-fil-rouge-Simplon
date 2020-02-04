e3714efad45fe46dcdd20426cbf673f613ea2dd7
PKZS476 <PKZS476@444240AC153.corp.log.intra.laposte.fr>
2020-02-04 16:54:58 +0100

    creation de la classe test de user

---
+++ backend/demo-1/src/test/java/fr/api/project/app/controller/UserControllerTest.java
@@ -0,0 +1,92 @@
+package fr.api.project.app.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * tests des requetes api du controlleur user
+ * @author didax
+ *
+ */
+class UserControllerTest 
+{
+
+	@Test
+	public final void testCreate() 
+	{
+		UserController userAdded = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testRead() 
+	{
+		UserController userReaded = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testReadAll() 
+	{
+		UserController allUserReaded = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testUpdate() 
+	{
+		UserController userUpdated = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testUpdateAll() 
+	{
+		UserController userUpdatedByAdmin = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testUpdateRanking() 
+	{
+		UserController userRankUpdate = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testDelete() 
+	{
+		UserController userDeleted = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testGetTypeUser() 
+	{
+		UserController userTypeGetted = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testAddTypeUser() 
+	{
+		UserController userTypeAdded = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testUpdateTypeUser() 
+	{
+		UserController userTypeUpdated = new UserController();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public final void testDeleteTypeUser() 
+	{
+		UserController userTypeDeleted = new UserController();
+		fail("Not yet implemented");
+	}
+
+}
