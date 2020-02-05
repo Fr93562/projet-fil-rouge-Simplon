c17e7a343a290c8a435575d49e664d16cc565df6
pkzs476 <pkzs476@444240AC153.corp.log.intra.laposte.fr>
2020-02-05 16:08:06 +0100

    test faq

--- backend/demo-1/src/test/java/fr/api/project/app/controller/FaqControllerTest.java
+++ backend/demo-1/src/test/java/fr/api/project/app/controller/FaqControllerTest.java
@@ -1,9 +1,25 @@
 package fr.api.project.app.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.api.project.app.model.entity.Faq;
 import fr.api.project.app.repository.FaqRepository;
 
 /**
@@ -11,20 +27,44 @@ import fr.api.project.app.repository.FaqRepository;
  * @author didax
  *
  */
+@SpringBootTest
+@AutoConfigureMockMvc
 class FaqControllerTest 
 {
+	@Autowired
+	MockMvc mockMvc;
+	
+	@MockBean
+	FaqRepository repoVirtuel;
+	
+	@Autowired 
+	private ObjectMapper objectMapper;
+	
+	JacksonTester<Faq> faqJacksonTester;
+	
+	@BeforeEach
+	public void setUp() 
+	{
+		JacksonTester.initFields(this, objectMapper);
+	}
+	 
 	@Test
 	public final void testAjoutFaq() 
 	{
-		FaqController faqAdded = new FaqController();		
 		fail("Not yet implemented");
+		
 	}
 	
 	@Test
-	public final void testAfficherFaq() 
+	public final void testAfficherFaq () throws Exception
 	{
-		FaqController faqDisplayed = new FaqController();		
-		fail("Not yet implemented");
+		//Faq faqDisplayed = new Faq();
+		List<Faq> allFaq = new ArrayList<Faq>();
+		allFaq.add(new Faq("règle du jeu","comme ça","1"));
+		allFaq.add(new Faq("jouer solo", "like this", "2"));
+		when(this.repoVirtuel.findAll()).thenReturn(allFaq);
+		
+		this.mockMvc.perform(get("/faq")).andExpect(status().isOk());
 	}
 	
 	@Test
