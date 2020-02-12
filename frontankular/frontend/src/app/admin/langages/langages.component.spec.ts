ea5e685ae64254aa2eb897693f2e2e11096a7538
ElodiePessy <56915683+ElodiePessy@users.noreply.github.com>
2020-02-12 10:36:02 +0100

    Commit admin
    Commit admin

--- frontankular/frontend/src/app/admin/langages/langages.component.spec.ts
+++ frontankular/frontend/src/app/admin/langages/langages.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminComponent } from './admin.component';
+import { LangagesQuestionsComponent } from './langages-questions.component';
 
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+describe('LangagesQuestionsComponent', () => {
+  let component: LangagesQuestionsComponent;
+  let fixture: ComponentFixture<LangagesQuestionsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [ LangagesQuestionsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
+    fixture = TestBed.createComponent(LangagesQuestionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
