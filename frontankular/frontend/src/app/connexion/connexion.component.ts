15bed748023315b66356cf5c9e629e0801e6740c
didaxlm <55740723+didaxlm@users.noreply.github.com>
2020-02-14 14:53:10 +0100

    Modif module connexion

--- frontankular/frontend/src/app/connexion/connexion.component.ts
+++ frontankular/frontend/src/app/connexion/connexion.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder} from '@angular/forms';
+import { FormBuilder, FormControl, Validators} from '@angular/forms';
 
+import { User } from '../shared/models/user';
+import { AuthentificationService } from '../shared/services/authentification.service';
+
+
+/**
+ * gère la connexion en récupérant les données du formulaire
+ * les traitants et l'envoie au service authentification 
+ * 
+ * @author Didax
+ */
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
@@ -8,13 +18,59 @@ import { FormBuilder} from '@angular/forms';
 })
 export class ConnexionComponent implements OnInit {
 
+  // attributs du formulaire
+  formUsername = new FormControl('',Validators.required);
+  formPassword = new FormControl('',Validators.required);
+
+  // attributs de data
+  private username: string = "";
+  private password: string = "";
   
-  
-  constructor(private fb: FormBuilder) { }
+  user : User = new User();
 
-  ngOnInit() {
-
-
+  constructor(private fb: FormBuilder, private auth : AuthentificationService) 
+  { 
+    
   }
+
+  ngOnInit() {}
+  
+  /**
+   * récupère les informations du formulaire et effectue une vérification
+   * et le stocke dans le composant
+   */
+  getData(){
+
+    this.username = this.formUsername.value;
+    this.password = this.formPassword.value;
+    let fake = "fake";
+
+    if (this.password.length > 255 || this.password.length < 6 || this.username.length > 30 || this.username.length < 4)
+    {
+      this.username = fake;
+      this.password = fake;
+    }
+  }
+
+  /**
+   * envoie les informations au service authentification
+   */
+  sendData() {
+
+    this.user.username = this.username;
+    this.user.password = this.password;
+    this.auth.login(this.user);
+  }
+
+  /**
+   * regroupe la réception et l'envoi des données
+   */
+  sendRequest() {
+
+    this.getData();
+    this.sendData();
+  }
+
+
 }
 
