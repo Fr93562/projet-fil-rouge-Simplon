6d5338e74e49d5fbd964857dc4a0878db6c2c32c
CamilleLP4 <56171925+CamilleLP4@users.noreply.github.com>
2020-02-19 15:37:04 +0100

    Fin jeu

--- frontankular/frontend/src/app/shared/services/game.service.ts
+++ frontankular/frontend/src/app/shared/services/game.service.ts
@@ -4,12 +4,19 @@ import { RequestService } from './request.service';
 import { DataService } from './data.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, concat } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Gere les appels en base pour jouer.component
+ */
 export class GameService extends RequestService {
 
+  /**
+   * Attributs
+   */
   listQuestions: Question[] = [];
   url = this.data.baseUrl + '/questions';
 
@@ -17,31 +24,44 @@ export class GameService extends RequestService {
     super(http, data);
   }
 
+  /**
+   * Recupere la liste de questions en relation avec les langages choisi
+   * Retourne un Observable contenant un tableau de Questions
+   * @param listLangages  langages selectionné
+   */
   private createListQuestions(listLangages: Array<string>): Observable<Question[]> {
     if (this.listQuestions.length > 0) {
       return of(this.listQuestions);
     } else {
-      return concat(...listLangages.map(langage => this.getTrivialCode(this.url + '?langage=' + langage)));
-    }
-
-  }
-
-  getQuestion(listLangages: Array<string>): Question {
-    if (this.listQuestions.length > 0) {
-      return this.selectQuestion();
-    } else {
-      this.createListQuestions(listLangages).subscribe(list => {
-        this.listQuestions = list;
-        return this.selectQuestion();
-      });
+      return concat(...listLangages.map(langage => this.getTrivialCode(this.url + '?langage=' + langage)))
+        .pipe(tap(serverConcatQuestions => this.listQuestions = serverConcatQuestions));
     }
   }
 
+  /**
+   * Renvoi un Observable contenant une Questions
+   * @param listLangages langages selectionné
+   */
+  getQuestion(listLangages: Array<string>): Observable<Question> {
+     return this.createListQuestions(listLangages).pipe(
+        map(list => this.selectQuestion())
+      );
+  }
+
+  /**
+   * Selectionne une question au hasard dans la liste
+   */
   private selectQuestion(): Question {
     const dice: number = Math.floor(Math.random() * this.listQuestions.length);
     const returnQuestion: Question = this.listQuestions[dice];
     this.listQuestions.splice(dice, 1);
-    console.log(returnQuestion);
     return returnQuestion;
+  }
+
+  /**
+   * Reinitialise le jeu
+   */
+  clear() {
+    this.listQuestions = [];
   }
 }
